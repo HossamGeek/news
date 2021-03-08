2d5fb0c04e8600e0cbbe6f64a3615fe209fe1443
Hossam Mahmoud <hossam_geekit@yahoo.com>
2021-03-08 16:34:10 -0700

    add navbar

--- component/nav.js
+++ component/nav.js
@@ -6,29 +6,18 @@ const NavBar = ()=>{
     return (
         <>
         <Navbar bg="light" expand="lg">
-        <Hidden lgDown>
-        <Navbar.Brand href="#home" className=".d-none .d-sm-block" >React-Bootstrap</Navbar.Brand>
-
-        </Hidden>
-    
+       
+        <Navbar.Brand href="#home" className="offset-lg-1 offset-md-0" >React-Bootstrap</Navbar.Brand>
+        
+        <Hidden > </Hidden>
             
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-                </Form>
+                
+                <div className="offset-lg-8">
+                    <Button  className="mr-sm-5">Login</Button>
+                    <Button className="ml-sm-5">Register</Button>
+                </div>
             </Navbar.Collapse>
             </Navbar>
             </>
